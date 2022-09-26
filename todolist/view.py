f4ed96d05972ee77e3a55ea26a1ca56a49c8a8ac
huynhdthanhtuan <tuan40655@gmail.com>
2022-09-26 21:46:34 +0700

    Home page: Add logic edit note

--- todolist/view.py
+++ todolist/view.py
@@ -1,62 +1,82 @@
-import json
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from . import db
 from .model import Note
+import json 
 
 view = Blueprint("view", __name__)
 
-@view.route("/", methods=["GET", "POST"])
+@view.route("/", methods=["GET"])
 @login_required
 def home():
-   if request.method == "POST":
-      note = request.form.get("note")
-
-      if len(note) < 1:
-         flash("Failed! Note is too short!")
-      else:
-         new_note = Note(data=note, user_id=current_user.id)
-         db.session.add(new_note)
-         db.session.commit()
-         flash("Add note successfully!")
-
    return render_template("index.html", user=current_user)
 
 
-# @view.route("/edit-note", methods=["POST"])
-# def edit_note():
-#    if request.method == "POST":
-#       note = request.body.get("edited-note")
+@view.route("/add-note", methods=["POST"])
+@login_required
+def add_note():
+   body_data = json.loads(request.data)
+   new_note = body_data['new_note']
 
-#       if len(note) < 1:
-#          flash("Failed! Note is too short!")
-#       else:
-#          edited_note = Note(data=note, user_id=current_user.id)
-#          # db.session.add(edited_note)
-#          db.session.commit()
-#          flash("Edit note successfully!")
+   if len(new_note) < 1:
+      flash("Failed! Note is too short!")
+   else:
+      try: 
+         new_note_data = Note(data=new_note, user_id=current_user.id)
+         db.session.add(new_note_data)
+         db.session.commit()
+      
+         flash("Add note successfully!")
+      except:
+         flash("Add note failed!")
 
-#    return render_template("index.html", user=current_user)
+   return jsonify({})
+
+
+@view.route("/edit-note", methods=["POST"])
+@login_required
+def edit_note():
+   body_data = json.loads(request.data)
+
+   note_id = body_data['note_id']
+   note_edit_value = body_data['note_edit_value']
+   note = Note.query.get(note_id)
+
+   if note:
+      if note.user_id == current_user.id:
+         try: 
+            note = Note.query.filter_by(id=note_id).first()
+            note.data = note_edit_value
+            db.session.commit()
+
+            flash("Edit note successfully!")
+         except:
+            flash("Edit note failed!")
+   else:
+      flash("Edit note failed!")
+
+   return jsonify({})
 
 
 @view.route("/delete-note", methods=["POST"])
+@login_required
 def delete_note():
-   if request.method == "POST":
-      body_data = json.loads(request.data)
+   body_data = json.loads(request.data)
 
-      note_id = body_data['note_id']
-      note = Note.query.get(note_id)
+   note_id = body_data['note_id']
+   note = Note.query.get(note_id)
 
-      if note:
-         if note.user_id == current_user.id:
-            try: 
-               db.session.delete(note)
-               db.session.commit()
-               flash("Delete note successfully!")
-               return jsonify({ "status": "sucessfully" })
-            except:
-               flash("Delete note failed!")
-               return jsonify({ "status": "failed" })
-      else:
-         flash("Delete note failed!")
-         return jsonify({ "status": "failed" })
+   if note:
+      if note.user_id == current_user.id:
+         try: 
+            db.session.delete(note)
+            db.session.commit()
+
+            flash("Delete note successfully!")
+         except:
+            flash("Delete note failed!")
+   else:
+      flash("Delete note failed!")
+
+   return jsonify({})
+
